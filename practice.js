16a68b4c646fc7e40916b9eee87a314d54dd9eb3
Mayra Landa <mlandae16@gmail.com>
2024-06-10 17:25:25 -0700

    add calculator

--- practice.js
+++ practice.js
@@ -6,9 +6,32 @@ function reverseString(string){
   return string.split("").reverse().join("");
 }
 
+const calculator = {
+  add: function(a, b){
+    return a + b;
+  },
+
+  subtract: function(a, b){
+    return a - b;
+  },
+
+  multiply: function(a, b){
+    return a * b;
+  },
+
+  divide: function(a, b){
+    if (b === 0){
+      throw new Error("Division by zero");
+    }
+    return a / b;
+  }
+  
+}
+
 module.exports = {
   capitalize,
-  reverseString
+  reverseString,
+  calculator
 };
 
 
--- practice.test.js
+++ practice.test.js
@@ -1,4 +1,4 @@
-const {capitalize, reverseString} = require("./practice");
+const {capitalize, reverseString, calculator} = require("./practice");
 
 test("capitalize should capitalize the first letter of a word", () => {
   expect(capitalize("hi")).toBe("Hi");
@@ -6,4 +6,24 @@ test("capitalize should capitalize the first letter of a word", () => {
 
 test("reverseString should reverse a string", () => {
   expect(reverseString("hi")).toBe("ih");
+});
+
+test("add function adds two numbers correctly", () => {
+  expect(calculator.add(5, 3)).toBe(8);
+})
+
+test("subtract function subtracts two numbers correctly", () => {
+  expect(calculator.subtract(5, 3)).toBe(2);
+})
+
+test("multiply function multiplies two numbers correctly", () => {
+  expect(calculator.multiply(5, 3)).toBe(15);
+})
+
+test("divide function divides two numbers correctly", () => {
+  expect(calculator.divide(10, 5)).toBe(2);
+});
+
+test("divide function throws error when dividing by zero", () => {
+  expect(() => calculator.divide(10, 0)).toThrowError("Division by zero");
 });
